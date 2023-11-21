59619095acb8e4e147409098789a080037abef77
Ted Cordeiro Jr <tedcordeirojr@gmail.com>
2023-11-21 02:18:55 -0300

    exercicio de função anonima

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 
 
-for (let i = 0;i <=10 ; i++){
-    const dobro = i*2; 
+// for (let i = 0;i <=10 ; i++){
+//     const dobro = i*2; 
 
 
-    console.log("o dobro do numero "+i+" é " +dobro);
+//     console.log("o dobro do numero "+i+" é " +dobro);
     
+// }
+
+
+function somar(numero1,numero2) {
+    return numero1 + numero2
+
 }
+let resultadoDasSoma = somar (10,10) 
+console.log(resultadoDasSoma)
