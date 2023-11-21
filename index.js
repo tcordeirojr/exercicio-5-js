a4ab080c8e4ea128540ee5afa31dcb8eabc6d47c
Ted Cordeiro Jr <tedcordeirojr@gmail.com>
2023-11-21 02:25:03 -0300

    usando função anonima

--- index.js
+++ index.js
@@ -15,3 +15,9 @@ function somar(numero1,numero2) {
 }
 let resultadoDasSoma = somar (10,10) 
 console.log(resultadoDasSoma)
+
+
+let resultadoDaSoma = function (numero1, numero2) {
+    return numero1 + numero2 
+}
+console.log(resultadoDaSoma(10,10))
