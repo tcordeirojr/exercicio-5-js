834aa982685b7a017f9e17b3424022c912a9e28a
Ted Cordeiro Jr <tedcordeirojr@gmail.com>
2023-11-21 02:41:57 -0300

    arrow functions

--- index.js
+++ index.js
@@ -21,3 +21,14 @@ let resultadoDaSoma = function (numero1, numero2) {
     return numero1 + numero2 
 }
 console.log(resultadoDaSoma(10,10))
+
+
+//Arrow Functions
+let outraSoma = (numero1, numero2) => {
+    return numero1 + numero2
+}
+console.log(outraSoma(12,15))
+
+
+const incentivarQuester = nomeQuester => console.log("Parabéns "+nomeQuester+", Você chegou ao módulo de JS intermediário")
+incentivarQuester("Ted") //maneira com menos código 
