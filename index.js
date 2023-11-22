90c3930b23fe510dc757ac4d0bb2f9b502561550
Ted Cordeiro Jr <tedcordeirojr@gmail.com>
2023-11-22 03:45:40 -0300

    exercicio de manipulação de eventos no dom

--- index.js
+++ index.js
@@ -5,40 +5,40 @@
 
 
 //     console.log("o dobro do numero "+i+" é " +dobro);
-    
+
 // }
 
 
-function somar(numero1,numero2) {
+function somar(numero1, numero2) {
     return numero1 + numero2
 
 }
-let resultadoDasSoma = somar (10,10) 
+let resultadoDasSoma = somar(10, 10)
 console.log(resultadoDasSoma)
 
 
 let resultadoDaSoma = function (numero1, numero2) {
-    return numero1 + numero2 
+    return numero1 + numero2
 }
-console.log(resultadoDaSoma(10,10))
+console.log(resultadoDaSoma(10, 10))
 
 
 //Arrow Functions
 let outraSoma = (numero1, numero2) => {
     return numero1 + numero2
 }
-console.log(outraSoma(12,15))
+console.log(outraSoma(12, 15))
 
 
-const incentivarQuester = nomeQuester => console.log("Parabéns "+nomeQuester+", Você chegou ao módulo de JS intermediário")
+const incentivarQuester = nomeQuester => console.log("Parabéns " + nomeQuester + ", Você chegou ao módulo de JS intermediário")
 incentivarQuester("Ted") //maneira com menos código 
 
 
 //Higher order functions 
-function multiplicar(multiplicador ) {
-    return function (numero){
-        return  numero * multiplicador
-    }    
+function multiplicar(multiplicador) {
+    return function (numero) {
+        return numero * multiplicador
+    }
 }
 
 let dobrar = multiplicar(2)
@@ -53,19 +53,55 @@ console.log(quadruplicar(6))
 
 //Manipulando atributos e estilos de dos elementos de DOM
 
-function marcarRadio(genero){
+function marcarRadio(genero) {
     let radioMasculino = document.getElementById('genero-masculino');
     let radioFeminino = document.getElementById("genero-feminino");
-    if(genero === "M"){
+    if (genero === "M") {
         radioMasculino.checked = true;
-    }else if(genero === "F"){
-        radioFeminino.checked = true; 
+    } else if (genero === "F") {
+        radioFeminino.checked = true;
     }
 }
 
 //eventos de DOM
-document
-.querySelector("select[name=estados]")
-.addEventListener('change',function(event){
-    console.log(event.target.value)
-});
+// document
+// .querySelector("select[name=estados]")
+// .addEventListener('change',function(event){
+//     console.log(event.target.value)
+// });
+
+
+//resolução de exercício intermediário 1
+// const botao = document.getElementById('botaoMudaCor');
+// const quadradoAzul = document.querySelector('.azul');
+
+// botao.addEventListener('click', trocardeCor)
+
+// function trocardeCor() {
+//     quadradoAzul.classList.remove('azul');
+//     quadradoAzul.classList.add('amarelo');
+        
+//     }
+
+    // const botao = document.getElementById('botaoMudaCor');
+    // const quadradoVermelho = document.querySelector('.vermelho');
+    // console.log(quadradoVermelho)
+    // botao.addEventListener('click', trocarCor);
+
+    // function trocarCor() {
+    //     quadradoVermelho.classList.remove('vermelho');
+    //     quadradoVermelho.classList.add('verde');
+    // }
+    
+const botao = document.getElementById('botaoMudaCor');
+const quadradoAzul = document.querySelector('.azul');
+const quadradoVermelho = document.querySelector('.vermelho');
+
+botao.addEventListener('click',trocarCor);
+
+function trocarCor(){
+    quadradoAzul.classList.remove('azul');
+    quadradoAzul.classList.add('amarelo');
+    quadradoVermelho.classList.remove('vermelho');
+    quadradoVermelho.classList.add('verde');
+}
